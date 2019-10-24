e864ca4ac3bb00999fa6ac68b024bad86f1fd56e
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-10-24 11:18:32 -0400

    test wip

--- src/bp/core/services/dialog/instruction/strategy.test.ts
+++ src/bp/core/services/dialog/instruction/strategy.test.ts
@@ -1,5 +1,78 @@
-describe('parseInstruction', () => {
-  test('undefined', () => {
-    expect(1).toEqual(1)
+import * as sdk from 'botpress/sdk'
+import _ from 'lodash'
+
+interface Variables {
+  user: object
+  session: object
+  temp: object
+  bot: object
+}
+
+const assignVariablesToState = (state: sdk.IO.EventState, variables: Variables) => {
+  _.assign(state.user, variables.user)
+}
+
+const buildEmptyState = () => {
+  return {
+    user: {},
+    session: {
+      lastMessages: []
+    },
+    temp: {},
+    bot: {},
+    context: {},
+    __stacktrace: []
+  }
+}
+
+const buildEmptyVariables = () => {
+  return {
+    user: {},
+    session: {},
+    temp: {},
+    bot: {}
+  }
+}
+
+describe('assignVariablesToState', () => {
+  test('empty state and variables', () => {
+    const state = buildEmptyState()
+    const variables = buildEmptyVariables()
+
+    assignVariablesToState(state, variables)
+
+    expect(state).toEqual(buildEmptyState())
+  })
+
+  test('non-empty state and variables', () => {
+    const state = buildEmptyState()
+    _.set(state, 'user.a', 1)
+    const variables = buildEmptyVariables()
+
+    assignVariablesToState(state, variables)
+
+    expect(state.user).toEqual({ a: 1 })
+  })
+
+  test('non-empty state and non-empty variables with same key', () => {
+    const state = buildEmptyState()
+    _.set(state, 'user.a', 1)
+    const variables = buildEmptyVariables()
+    _.set(variables, 'user.a', 1)
+
+    assignVariablesToState(state, variables)
+
+    expect(state.user).toEqual({ a: 1 })
+  })
+
+  test('non-empty state and non-empty variables with different keys', () => {
+    const state = buildEmptyState()
+    _.set(state, 'user.a', 1)
+    const variables = buildEmptyVariables()
+    _.set(variables, 'user.b', 1)
+
+    assignVariablesToState(state, variables)
+
+    expect(state.user).toEqual({ a: 1, b: 1 })
   })
 })
