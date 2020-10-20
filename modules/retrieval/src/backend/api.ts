5d47b9931319a9f8c9033b8e239ac082e9f3f6ce
Pierre Snell <pierre.snell@botpress.com>
2020-10-20 10:47:28 -0400

    starting work

--- modules/retrieval/src/backend/api.ts
+++ modules/retrieval/src/backend/api.ts
@@ -1,21 +1,21 @@
 import * as sdk from 'botpress/sdk'
 import _ from 'lodash'
 
-import { rerank } from './reranker'
-import { inferQuestion } from './retriever'
-import { getSvmProb } from './svmProbas'
 
 export default async (bp: typeof sdk, state) => {
   const router = bp.http.createRouterForBot('retrieval')
 
-  router.post('/inferQuestion', async (req, res) => {
-    const axiosConfig = await bp.http.getAxiosConfigForBot(req.params.botId, { localUrl: true })
-    res.send(await inferQuestion(req.body.question, state, axiosConfig))
+  router.post('/askQuestion', async (req, res) => {
+    res.send(await askQuestion(req.body.question, state))
   })
 
-  router.get('/getSvmProb', async (req, res) => {
-    const axiosConfig = await bp.http.getAxiosConfigForBot(req.params.botId, { localUrl: true })
-    res.send(await getSvmProb(state, axiosConfig))
+  router.post('/import', async (req, res) => {
+    // add to bm25 index
+    // add to embeddings index
+
+  })
+
+  router.get('/upload-status/:status-id', async (req, res) => {
   })
 
 }
--- modules/retrieval/src/views/full/index.tsx
+++ modules/retrieval/src/views/full/index.tsx
@@ -1,93 +1,77 @@
 import { Button, Checkbox, Icon, Intent, Tooltip } from '@blueprintjs/core'
+import { confirmDialog, Icons, lang, toast } from 'botpress/shared'
 import { Container } from 'botpress/ui'
-import React, { useEffect, useState } from 'react'
-
-import { feedback, kb_entry } from '../../backend/typings'
 import _ from 'lodash'
-import style from './style.scss'
+import React, { useState } from 'react'
 
-const NewQnA = props => {
+
+const Retrieval = props => {
   const [question, setQuestion] = useState('')
-  const [new_model_name, setNewModelName] = useState('')
-  const [pythonImportLogs, setPythonImportLogs] = useState('')
-  const [closestQuestions, setClosestQuestions] = useState([])
-  const [svmProb, setSvmProb] = useState({ exemples: [], min: 0, max: 0, mean: 0 } as {
-    exemples:
-    {
-      confidence: number,
-      utterance: string,
-      answer: string,
-      eval: number,
-    }[],
-    min: number,
-    max: number,
-    mean: number
-  })
-  const [results, setResults] = useState([
-    {
-      question: '',
-      bp_bot: '',
-      deep_bot: '',
-      bp_right: false,
-      deep_right: false,
-      winner: '',
-      confidence: 0,
-      deep_confidence: 0
+  const [answers, setAnswers] = useState([''])
+  const askQuestion = async () => {
+    const { data } = await props.bp.axios.post('/mod/retrieval/askQuestion', { question })
+    setAnswers([data])
+  }
+
+
+  const askFileConfirmation = async uploadFile => {
+    if (
+      await confirmDialog(`${uploadFile.name} : Are you sure to import ?`, {
+        acceptLabel: lang.tr('ok'),
+        declineLabel: lang.tr('cancel')
+      })
+    ) {
+      await importFile(uploadFile)
     }
-  ])
-  const [answer, setAnswer] = useState([
-    {
-      orig: '',
-      content: '',
-      embedding: [],
-      contexts: [],
-      confidence: 0,
-      feedbacks: [{ utterance: '', polarity: 0, reranked: false } as feedback],
-      winner: "botpress" || "deep"
+  }
+
+  const importFile = async file => {
+    const extension = file.name.split('.').slice(-1)[0]
+    if (extension !== 'txt') {
+      toast.failure('bad file format')
+      return
     }
-  ])
 
-  const inferQuestion = async () => {
-    const { data } = await props.bp.axios.post('/mod/new_qna/inferQuestion', { question })
-    console.log(data)
-    setAnswer(data)
-  }
-  const runTests = async () => {
-    const { data } = await props.bp.axios.get('/mod/new_qna/runTests')
-    setResults(data)
-  }
-  const importNewOnnxModel = async () => {
-    const { data } = await props.bp.axios.post('/mod/new_qna/importOnnxModels', { new_model_name })
-    console.log(data)
-    setPythonImportLogs(data)
-  }
+    let intervalHandle: number
+    try {
+      const form = new FormData()
+      form.append('file', file)
 
-  const electClosestQuestions = async () => {
-    const { data } = await props.bp.axios.post('/mod/new_qna/electClosestQuestions', { question })
-    setClosestQuestions(data)
-  }
+      const { data } = await props.bp.axios.post(`/mod/retrieval/import`, form, props.bp.axiosConfig)
+      const uploadStatusId = data
 
-  const getSvmProb = async () => {
-    const { data } = await props.bp.axios.get('/mod/new_qna/getSvmProb')
-    console.log(data)
-    setSvmProb(data)
+      intervalHandle = window.setInterval(async () => {
+        const { data } = await props.bp.axios.get(`/mod/retrieval/upload-status/${uploadStatusId}`)
+
+        if (data === 'sucess') {
+          clearInterval(intervalHandle)
+          toast.success('Upload sucessful')
+        } else if (data !== 'uploading' && data !== 'sucess') {
+          toast.failure('Upload crashed')
+          clearInterval(intervalHandle)
+        }
+      }, 500)
+    } catch (err) {
+      clearInterval(intervalHandle)
+      toast.failure(err.message)
+    }
   }
-  useEffect(() => { }, [])
 
   return (
     <Container sidePanelHidden>
       <div />
-      <div className={style.main}>
+      <div>
+        <p>Information Retrieval module !</p>
+        <p>Upload a file</p>
         <input
-          type="text"
-          value={new_model_name}
-          onChange={event => {
-            setNewModelName(event.target.value)
+          type="file"
+          onChange={async e => {
+            if ((e.target as HTMLInputElement).files) {
+              await askFileConfirmation((e.target as HTMLInputElement).files[0])
+            }
           }}
         />
-        <button onClick={importNewOnnxModel}>Import new Onnx model</button>
-        <div>{pythonImportLogs}</div>
-        <h1>Covid Bot</h1>
+        <p>Ask a question</p>
         <input
           type="text"
           value={question}
@@ -95,146 +79,14 @@ const NewQnA = props => {
             setQuestion(event.target.value)
           }}
         />
-        <button onClick={inferQuestion}>Ask Question</button>
-        <button onClick={runTests}>Run Tests</button>
-        <button onClick={electClosestQuestions}>Elect Closests questions</button>
-        <button onClick={getSvmProb}>Get Svm probs on bad utterances</button>
-        <h2>Svm Probs</h2>
-        <div>
-          <p> Mean {svmProb.mean}</p>
-          <p> Max {svmProb.max} </p>
-          <p> Min {svmProb.min}</p>
-          <p> Exemples </p>
-
-          <table>
-            <tbody>
-              <tr>
-                <td>Question</td>
-                <td>Confidence</td>
-                <td>Answer</td>
-                <td>Incorrect</td>
-              </tr>
-              {svmProb.exemples.sort((a, b) => (a.eval > b.eval) ? -1 : 1).slice(0, 200).map(o => {
-                return (
-                  <tr>
-                    <td>{o.utterance}</td>
-                    <td>{o.confidence}</td>
-                    <td>{o.answer}</td>
-                    <td>{o.eval}</td>
-                  </tr>)
-              })}
-            </tbody>
-          </table>
-        </div>
-        <h2>Top 3 Answers</h2>
-        <div>
-          <ul>
-            {closestQuestions.map(q => {
-              return (
-                <li>
-                  {q.utterance} _ {q.confidence}
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-        <div>
-          {answer.map((e, i) => {
-            return (
-              <div>
-                <h4>Document {i} </h4>
-                <h5>Origine</h5>
-                <p>{e.orig} </p>
-                <h5>Content &nbsp;&nbsp;{e.confidence}</h5>
-                <p>{e.content} </p>
-                <h5>Contexts</h5>
-                <p>{e.contexts} </p>
-                <h5> Winner </h5>
-                <p> {e.winner} </p>
-
-                {e.feedbacks.map((e, i) => {
-                  return (
-                    <div>
-                      <h5 className={style.inline}>Feedback {i}</h5>
-                      <p>
-                        {e.reranked}&nbsp;&nbsp;
-                        {e.polarity}&nbsp;&nbsp;
-                        {e.utterance}
-                      </p>
-                    </div>
-                  )
-                })}
-              </div>
-            )
-          })}
-        </div>
-        <div>
-          <h1>Custom tests </h1>
-          <table>
-            <tbody>
-              <tr>
-                <td>Question </td>
-                <td>Bp</td>
-                <td>Deep</td>
-                <td>RBp</td>
-                <td>RDeep</td>
-                <td>Conf</td>
-                <td>DConf</td>
-                <td>Winner</td>
-              </tr>
-              {results.map((r, i) => {
-                return (
-                  <tr>
-                    <td>{r.question}</td>
-                    <td>{r.bp_bot}</td>
-                    <td>{r.deep_bot}</td>
-                    <td>
-                      <Checkbox
-                        checked={results[i].bp_right}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                          results[i].bp_right = e.target.checked
-                          setResults([...results])
-                        }}
-                      ></Checkbox>
-                    </td>
-                    <td>
-                      <Checkbox
-                        checked={results[i].deep_right}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                          results[i].deep_right = e.target.checked
-                          setResults([...results])
-                        }}
-                      ></Checkbox>
-                    </td>
-                    <td>{_.round(r.confidence, 2)}</td>
-                    <td>{_.round(r.deep_confidence, 2)}</td>
-                    <td>{r.winner}</td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
-          <p>
-            BP right {results.filter(e => e.bp_right).length} / {results.length}
-          </p>
-          <p>
-            Deep right {results.filter(e => e.deep_right).length} / {results.length}
-          </p>
-          <p>
-            BP Better : {results.filter(e => e.bp_right && !e.deep_right).length} / {results.length}
-          </p>
-          <p>
-            Deep Better : {results.filter(e => e.deep_right && !e.bp_right).length} / {results.length}
-          </p>
-          <p>
-            Repartition BP : {results.filter(e => e.winner == "botpress").length} / {results.length}
-          </p>
-          <p>
-            Repartition Deep : {results.filter(e => e.winner == "deep").length} / {results.length}
-          </p>
-        </div>
+        <button onClick={askQuestion}>Ask Question</button>
+      </div>
+      <div>
+        <ul>
+          {answers.map((a, i) => {return (<li>{a} {i}</li>)})}
+        </ul>
       </div>
     </Container>
   )
 }
-export default NewQnA
+export default Retrieval
