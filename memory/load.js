22c969a0aa3b5bc8a024c4b087e374577dcc28e6
Sylvain Perron <sylvain.perron@botpress.com>
2020-12-05 17:37:56 -0500

    run - Not calling converse

--- memory/load.js
+++ memory/load.js
@@ -2,7 +2,7 @@ import http from 'k6/http'
 import { sleep } from 'k6'
 
 export default function() {
-  http.post(`http://localhost:3000/api/v1/bots/test/converse/user${__VU}`, {
+  http.post(`http://localhost:3000/api/v1/bots/test/converse/test/user${__VU}`, {
     type: 'text',
     text: `Hey! from ${__VU}`
   })
