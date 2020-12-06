8e107e9378b12fcc5956d11fe59723bcbaea7e52
Sylvain Perron <sylvain.perron@botpress.com>
2020-12-05 19:23:00 -0500

    run - converse api restore

--- src/bp/core/services/converse.ts
+++ src/bp/core/services/converse.ts
@@ -109,20 +109,18 @@ export class ConverseService {
       }
     })
 
-    // const userKey = buildUserKey(botId, userId)
-    // const timeoutPromise = this._createTimeoutPromise(botId, userKey)
-    // const donePromise = this._createDonePromise(userKey)
+    const userKey = buildUserKey(botId, userId)
+    const timeoutPromise = this._createTimeoutPromise(botId, userKey)
+    const donePromise = this._createDonePromise(userKey)
 
     await this.eventEngine.sendEvent(incomingEvent)
 
-    // return Promise.race([timeoutPromise, donePromise]).finally(() => {
-    //   converseApiEvents.removeAllListeners(`done.${userKey}`)
-    //   converseApiEvents.removeAllListeners(`action.start.${userKey}`)
-    //   converseApiEvents.removeAllListeners(`action.end.${userKey}`)
-    //   delete this._responseMap[userKey]
-    // })
-
-    return { text: 'mocked converse' }
+    return Promise.race([timeoutPromise, donePromise]).finally(() => {
+      converseApiEvents.removeAllListeners(`done.${userKey}`)
+      converseApiEvents.removeAllListeners(`action.start.${userKey}`)
+      converseApiEvents.removeAllListeners(`action.end.${userKey}`)
+      delete this._responseMap[userKey]
+    })
   }
 
   private async _createDonePromise(userKey: string) {
